6264a1179943c18821604963e01032c8dce2ad39
w01f-o <arsenimarchenko@bk.ru>
2025-01-27 19:52:35 +0300

    ...

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -2,7 +2,10 @@ import { telegramBot } from '../index.js';
 
 export const sendTelegramMessage = async (message: string) => {
   try {
-    await telegramBot.telegram.sendMessage(449255241, message);
+    await telegramBot.telegram.sendMessage(
+      process.env.TELEGRAM_CHAT_ID!,
+      message
+    );
   } catch (error) {
     console.error('Ошибка при отправке сообщения в Telegram', error);
   }
