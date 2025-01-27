bbf5517edae3e5f7e5538b7cca121a536cb1f690
w01f-o <arsenimarchenko@bk.ru>
2025-01-27 19:59:38 +0300

    ...

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -2,10 +2,11 @@ import { telegramBot } from '../index.js';
 
 export const sendTelegramMessage = async (message: string) => {
   try {
-    await telegramBot.telegram.sendMessage(
-      process.env.TELEGRAM_CHAT_ID!,
-      message
-    );
+    const chatIds = process.env.TELEGRAM_CHAT_ID!.trim().split(',').map(Number);
+
+    for (const chatId of chatIds) {
+      await telegramBot.telegram.sendMessage(chatId, message);
+    }
   } catch (error) {
     console.error('Ошибка при отправке сообщения в Telegram', error);
   }
